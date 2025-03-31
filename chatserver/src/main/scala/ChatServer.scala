983f24d8a11b490dc41cd285ff45b5595e5350ec
Anzori (Nika) Gurchumelia <aghurtchumelia@Anzoris-MacBook-Pro.local>
2025-03-31 22:33:12 +0200

    implement chat-ui

--- chatserver/src/main/scala/ChatServer.scala
+++ chatserver/src/main/scala/ChatServer.scala
@@ -27,6 +27,7 @@ import PubSubMessage.Args.codecs._
 import PubSubMessage._
 import domain.{ChatParticipant, User}
 import json.Syntax.{JsonReadsSyntax, JsonWritesSyntax}
+import org.http4s.server.middleware.CORS
 import users.UserStatusManager
 
 import scala.concurrent.duration.DurationInt
@@ -104,133 +105,135 @@ object ChatServer extends IOApp.Simple {
   ): HttpApp[IO] = {
     val dsl = new Http4sDsl[IO] {}
     import dsl._
-    HttpRoutes.of[IO] {
-      case GET -> Root / "user" / "join" / username =>
-        (generateRandomId, generateRandomId)
-          .flatMapN { case (userId, chatId) =>
-            for {
-              _ <- IO.println(s"Registering $username in the system")
-              _ <- chatHistoryRef
-                .update(_.updated(chatId, ChatHistory.init(username, userId, chatId)))
-                .flatTap(cache => IO.println(s"ChatHistory cache: $cache"))
-              topic <- Topic[IO, Message]
-              _ <- chatTopicRef
-                .update(_.updated(chatId, topic))
-                .flatTap(cache => IO.println(s"ChatTopics cache: $cache"))
-              _ <- IO.println(s"Registered $username in the system")
-              response <- Ok(Registered(userId, username, chatId).toJson)
-            } yield response
+    CORS {
+      HttpRoutes.of[IO] {
+        case GET -> Root / "user" / "join" / username =>
+          (generateRandomId, generateRandomId)
+            .flatMapN { case (userId, chatId) =>
+              for {
+                _ <- IO.println(s"Registering $username in the system")
+                _ <- chatHistoryRef
+                  .update(_.updated(chatId, ChatHistory.init(username, userId, chatId)))
+                  .flatTap(cache => IO.println(s"ChatHistory cache: $cache"))
+                topic <- Topic[IO, Message]
+                _ <- chatTopicRef
+                  .update(_.updated(chatId, topic))
+                  .flatTap(cache => IO.println(s"ChatTopics cache: $cache"))
+                _ <- IO.println(s"Registered $username in the system")
+                response <- Ok(Registered(userId, username, chatId).toJson)
+              } yield response
+            }
+        case GET -> Root / "chat" / chatId =>
+          val maybeChatHistory = findById(chatHistoryRef)(chatId)
+          val maybeTopic = findById(chatTopicRef)(chatId)
+
+          val receiveF: IO[WebSocketFrameFlow] = maybeTopic.map {
+            case Some(topic) =>
+              topic.publish.compose[WebSocketFrames](_.evalMap { case Text(body, _) =>
+                body.as[ClientWsMsg] match {
+                  case Left(error) => IO.println(s"could not deserialize $body: $error") as UnrecognizedMessage
+                  case Right(clientWsMsg) =>
+                    clientWsMsg.args match {
+                      case userPong @ Pong(ChatParticipant.User) =>
+                        IO.realTimeInstant.flatMap(now =>
+                          updatePingPongRef(
+                            pingPongRef = pingPongRef,
+                            chatId = chatId,
+                            update = _.copy(userTimeStamp = now.some),
+                            initial = PingPong.initUser(now),
+                          ) as userPong,
+                        )
+                      case supportPong @ Pong(ChatParticipant.Support) =>
+                        IO.realTimeInstant.flatMap(now =>
+                          updatePingPongRef(
+                            pingPongRef = pingPongRef,
+                            chatId = chatId,
+                            update = _.copy(supportTimestamp = now.some),
+                            initial = PingPong.initSupport(now),
+                          ) as supportPong,
+                        )
+                      case Join(ChatParticipant.User, userId, username, None, None) =>
+                        for {
+                          _ <- IO.println(s"User with userId: $userId is attempting to join the chat server")
+                          user = User(username, userId, chatId)
+                          pubSubMsgAsJson = PubSubMessage.from(PubSubMessage.Args.UserPending(user)).toJson
+                          _ <- IO.println(s"Writing $user into Redis SortedSet with Score 0 - pending")
+                          _ <- userStatusManager.setPending(user.toJson)
+                          _ <- IO.println(s"""Publishing $pubSubMsgAsJson into Redis pub/sub "users" channel""")
+                          _ <- Stream.emit(pubSubMsgAsJson).through(redisPublisher.pipe).compile.drain
+                        } yield UserJoined(user)
+                      // User re-joins (browser refresh), so we load chat history
+                      // TODO: consider storing chat history in the browser local storage
+                      case Join(ChatParticipant.User, _, _, Some(_), _) =>
+                        maybeChatHistory.map(_.getOrElse(ChatExpired(chatId)))
+                      // Support joins for the first time
+                      case Join(ChatParticipant.Support, userId, username, None, Some(su)) =>
+                        for {
+                          _ <- IO.println(s"Support attempting to join user with userId: $userId")
+                          id <- generateRandomId
+                          support = domain.Support(id, su, User(username, userId, chatId))
+                          pubSubMsgAsJson = PubSubMessage.from(PubSubMessage.Args.SupportJoined(support)).toJson
+                          _ <- Stream.emit(pubSubMsgAsJson).through(redisPublisher.pipe).compile.drain
+                          _ <- IO.println(s"Support joined the user with userId: $userId")
+                        } yield SupportJoined(support)
+                      // Support re-joins (browser refresh), so we load chat history
+                      // TODO: consider storing chat history in the browser local storage
+                      case Join(ChatParticipant.Support, _, _, Some(_), Some(_)) =>
+                        maybeChatHistory.map(_.getOrElse(ChatExpired(chatId)))
+                      // chat message either from user or support
+                      case msg: ChatMessage =>
+                        for {
+                          now <- IO.realTimeInstant
+                          _ <- IO.println(s"participant:${msg.from} sent message:${msg.content} to:${msg.from.mirror} at:$now")
+                          msgWithTimeStamp = msg.copy(timestamp = Some(now))
+                          _ <- maybeChatHistory.flatMap {
+                            case Some(messages) =>
+                              chatHistoryRef
+                                .update(_.updated(chatId, messages + msgWithTimeStamp))
+                                .flatTap(cache => IO.println(s"ChatHistory cache: $cache"))
+                                .as(msgWithTimeStamp)
+                            case None => IO.pure(ChatExpired(chatId))
+                          }
+                        } yield msgWithTimeStamp
+                    }
+                }
+              })
+            case None => _ => Stream.empty
           }
-      case GET -> Root / "chat" / chatId =>
-        val maybeChatHistory = findById(chatHistoryRef)(chatId)
-        val maybeTopic = findById(chatTopicRef)(chatId)
 
-        val receiveF: IO[WebSocketFrameFlow] = maybeTopic.map {
-          case Some(topic) =>
-            topic.publish.compose[WebSocketFrames](_.evalMap { case Text(body, _) =>
-              body.as[ClientWsMsg] match {
-                case Left(error) => IO.println(s"could not deserialize $body: $error") as UnrecognizedMessage
-                case Right(clientWsMsg) =>
-                  clientWsMsg.args match {
-                    case userPong @ Pong(ChatParticipant.User) =>
-                      IO.realTimeInstant.flatMap(now =>
-                        updatePingPongRef(
-                          pingPongRef = pingPongRef,
-                          chatId = chatId,
-                          update = _.copy(userTimeStamp = now.some),
-                          initial = PingPong.initUser(now),
-                        ) as userPong,
-                      )
-                    case supportPong @ Pong(ChatParticipant.Support) =>
-                      IO.realTimeInstant.flatMap(now =>
-                        updatePingPongRef(
-                          pingPongRef = pingPongRef,
-                          chatId = chatId,
-                          update = _.copy(supportTimestamp = now.some),
-                          initial = PingPong.initSupport(now),
-                        ) as supportPong,
-                      )
-                    case Join(ChatParticipant.User, userId, username, None, None) =>
-                      for {
-                        _ <- IO.println(s"User with userId: $userId is attempting to join the chat server")
-                        user = User(username, userId, chatId)
-                        pubSubMsgAsJson = PubSubMessage.from(PubSubMessage.Args.UserPending(user)).toJson
-                        _ <- IO.println(s"Writing $user into Redis SortedSet with Score 0 - pending")
-                        _ <- userStatusManager.setPending(user.toJson)
-                        _ <- IO.println(s"""Publishing $pubSubMsgAsJson into Redis pub/sub "users" channel""")
-                        _ <- Stream.emit(pubSubMsgAsJson).through(redisPublisher.pipe).compile.drain
-                      } yield UserJoined(user)
-                    // User re-joins (browser refresh), so we load chat history
-                    // TODO: consider storing chat history in the browser local storage
-                    case Join(ChatParticipant.User, _, _, Some(_), _) =>
-                      maybeChatHistory.map(_.getOrElse(ChatExpired(chatId)))
-                    // Support joins for the first time
-                    case Join(ChatParticipant.Support, userId, username, None, Some(su)) =>
-                      for {
-                        _ <- IO.println(s"Support attempting to join user with userId: $userId")
-                        id <- generateRandomId
-                        support = domain.Support(id, su, User(username, userId, chatId))
-                        pubSubMsgAsJson = PubSubMessage.from(PubSubMessage.Args.SupportJoined(support)).toJson
-                        _ <- Stream.emit(pubSubMsgAsJson).through(redisPublisher.pipe).compile.drain
-                        _ <- IO.println(s"Support joined the user with userId: $userId")
-                      } yield SupportJoined(support)
-                    // Support re-joins (browser refresh), so we load chat history
-                    // TODO: consider storing chat history in the browser local storage
-                    case Join(ChatParticipant.Support, _, _, Some(_), Some(_)) =>
-                      maybeChatHistory.map(_.getOrElse(ChatExpired(chatId)))
-                    // chat message either from user or support
-                    case msg: ChatMessage =>
-                      for {
-                        now <- IO.realTimeInstant
-                        _ <- IO.println(s"participant:${msg.from} sent message:${msg.content} to:${msg.from.mirror} at:$now")
-                        msgWithTimeStamp = msg.copy(timestamp = Some(now))
-                        _ <- maybeChatHistory.flatMap {
-                          case Some(messages) =>
-                            chatHistoryRef
-                              .update(_.updated(chatId, messages + msgWithTimeStamp))
-                              .flatTap(cache => IO.println(s"ChatHistory cache: $cache"))
-                              .as(msgWithTimeStamp)
-                          case None => IO.pure(ChatExpired(chatId))
-                        }
-                      } yield msgWithTimeStamp
-                  }
-              }
-            })
-          case None => _ => Stream.empty
-        }
-
-        val sendF: IO[WebSocketFrames] = maybeTopic.map {
-          _.fold[WebSocketFrames](Stream.empty) {
-            _.subscribe(10)
-              .collect { case o: Out => Text(ServerWsMsg(o).toJson) }
-              .merge {
-                // send "ping" message to WS client every 500 millis
-                val ping = Text("ping")
-                Stream.emit(ping) ++
-                  Stream.awakeEvery[IO](500.millis) as ping
-              }
+          val sendF: IO[WebSocketFrames] = maybeTopic.map {
+            _.fold[WebSocketFrames](Stream.empty) {
+              _.subscribe(10)
+                .collect { case o: Out => Text(ServerWsMsg(o).toJson) }
+                .merge {
+                  // send "ping" message to WS client every 500 millis
+                  val ping = Text("ping")
+                  Stream.emit(ping) ++
+                    Stream.awakeEvery[IO](500.millis) as ping
+                }
+            }
           }
-        }
 
-        val receive: WebSocketFrames => Stream[IO, Unit] = (frames: WebSocketFrames) => Stream.eval(receiveF).flatMap(_(frames))
-        val send: Stream[IO, WebSocketFrame] = Stream.eval(sendF).flatten
+          val receive: WebSocketFrames => Stream[IO, Unit] = (frames: WebSocketFrames) => Stream.eval(receiveF).flatMap(_(frames))
+          val send: Stream[IO, WebSocketFrame] = Stream.eval(sendF).flatten
 
-        wsb
-          .withOnClose {
-            Console[IO].println("WS connection was closed") *>
-              (maybeTopic, pingPongRef.get.map(_.get(chatId))).flatMapN {
-                case (Some(topic), pingPong) =>
-                  // semantically blocks to make sure that we check the latest pong from User & Support later
-                  // in this 1.5 seconds the pingPong will be updated by the "pong" responses from the chat participant who didn't leave the conversation yet
-                  // so in the end, we will compare the timestamps of last "pong" messages for user and support.
-                  Temporal[IO].sleep(1500.millis) *>
-                    WsConnectionClosedAction
-                      .of[IO](chatHistoryRef, userStatusManager, redisPublisher)
-                      .react(topic, chatId, pingPong)
-                case _ => ().pure[IO]
-              }
-          }
-          .build(send, receive)
+          wsb
+            .withOnClose {
+              Console[IO].println("WS connection was closed") *>
+                (maybeTopic, pingPongRef.get.map(_.get(chatId))).flatMapN {
+                  case (Some(topic), pingPong) =>
+                    // semantically blocks to make sure that we check the latest pong from User & Support later
+                    // in this 1.5 seconds the pingPong will be updated by the "pong" responses from the chat participant who didn't leave the conversation yet
+                    // so in the end, we will compare the timestamps of last "pong" messages for user and support.
+                    Temporal[IO].sleep(1500.millis) *>
+                      WsConnectionClosedAction
+                        .of[IO](chatHistoryRef, userStatusManager, redisPublisher)
+                        .react(topic, chatId, pingPong)
+                  case _ => ().pure[IO]
+                }
+            }
+            .build(send, receive)
+      }
     }
   }.orNotFound
 }
